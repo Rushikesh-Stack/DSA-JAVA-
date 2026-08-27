dcfa29c3124133c13a1a07dcb422f779b20e386f
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-08-27 07:55:13 +0530

    Time: 27 ms (11.97%) | Memory: 47.2 MB (11.73%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,42 +1,70 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] a, int target) 
     {
-        
         List<List<Integer>> list=new ArrayList<>();
+        
+        Arrays.sort(a);
         int n=a.length;
-
+        int left;
+        int right;
+      
         for(int i=0;i<n;i++)
         {
+            
             int fixed1=a[i];
             for(int j=i+1;j<n;j++)
             {
-                HashMap<Long,Integer>map=new HashMap<>();
+                left= j+1;
+                right= n-1;
+               while(left<right)
+             {
                 int fixed2=a[j];
-                for(int k=j+1;k<n;k++)
+                long sum=(long) fixed1+fixed2+a[left]+a[right];
+                if(sum==target)
                 {
-                    long needed=(long) target-fixed1-fixed2-a[k];
+                    List<Integer> quad=new ArrayList<>();
+                    quad.add(fixed1);
+                    quad.add(fixed2);
+                    quad.add(a[left]);
+                    quad.add(a[right]);
 
-                    if(!map.containsKey(needed))
+                    if(!list.contains(quad))
                     {
-                        map.put((long) a[k],k);
+                         list.add(quad);
                     }
-                    else{
-                        List<Integer>quad=new ArrayList<>();
 
-                        quad.add(fixed1);
-                        quad.add(fixed2);
-                        quad.add((int)needed);
-                        quad.add(a[k]);
-                        
-                        Collections.sort(quad);
-                         if(!list.contains(quad))
-                            {
-                                list.add(quad);
-                            }
+                    left++;
+                    right--;
 
-                    }
+                while(left<right && a[left]==a[left-1])
+               {
+                left++;
+               }
+
+                while(left<right && a[right]==a[right+1])
+               {
+                 right--;
+               }
+
+               }
+
+               
+               else if(sum<target)
+                {
+                    left++;
                 }
+                else{
+                    right--;
+                }
+
+               
+
+               
+             }
+                
             }
+
+
         }
         
 
