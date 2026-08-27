655a799b8cf8794a50af0dea7808f32898926fa7
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-08-27 23:08:44 +0530

    Time: 344 ms (5.82%) | Memory: 47.8 MB (27.48%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int mostFrequentEven(int[] a) 
+    {
+        int n=a.length;
+        int cnt=0;
+        int maxcnt=0;
+        int ans=-1;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j] && a[j] %2==0)
+                {
+                    cnt++;
+                }
+            }
+
+            if(maxcnt<cnt)
+            {
+                ans=a[i]; //store previous element
+                maxcnt=Math.max(maxcnt,cnt);
+            }
+            else if(maxcnt==cnt)
+            {
+                //compare prevoius element with current element
+                if(a[i]<ans)
+                {
+                    ans=a[i]; //store smallest element
+                }
+            }
+
+            cnt=0;
+            
+        }
+
+        return ans;
+    }
+}
