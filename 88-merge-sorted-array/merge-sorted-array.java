cce41ad6e0862f92ac591c2507630ea3d6f72f26
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-08-29 09:07:38 +0530

    Time: 2 ms (24.39%) | Memory: 43.7 MB (78.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,12 +1,38 @@
 class Solution {
-    public void merge(int[] a1, int m, int[] a2, int n) 
-    {
-        
-        for(int i=0;i<n;i++)
+    public void merge(int[] a1, int m, int[] a2, int n) {
+
+        // If a2 is empty, nothing to merge
+        if(n == 0)
+            return;
+
+        // Compare elements of a1 with the smallest element of a2
+        for(int i = 0; i < m; i++)
         {
-           a1[m+i]=a2[i]; 
+            if(a1[i] > a2[0])
+            {
+                // Swap
+                int temp = a1[i];
+                a1[i] = a2[0];
+                a2[0] = temp;
+
+                // Restore sorted order of a2
+                int j = 0;
+
+                while(j < n - 1 && a2[j] > a2[j + 1])
+                {
+                    int temp2 = a2[j];
+                    a2[j] = a2[j + 1];
+                    a2[j + 1] = temp2;
+
+                    j++;
+                }
+            }
         }
 
-        Arrays.sort(a1);
+        // Copy a2 into the empty space of a1
+        for(int i = 0; i < n; i++)
+        {
+            a1[m + i] = a2[i];
+        }
     }
 }
