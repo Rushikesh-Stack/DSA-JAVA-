9fe85d8cd4d2722df0d27f93b8fed87aaa3ea57f
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-09-02 08:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (35.83%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,40 +1,12 @@
 class Solution {
-    public boolean uniformArray(int[] a1) 
-    {
-        int n=a1.length;
-        int a2[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            if(a1[i]%2==0)
-            {
-                a2[i]=a1[i];
-            }
-            else{
-                a1[i]++;
-                a2[i]=a1[i];
+    public boolean uniformArray(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                return true;
             }
         }
 
-        //check for all it all are even or odds
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            if(a1[i]%2==0)
-            {
-                 cnt++;  
-            }
-            else {
-                break;
-            }
-
-        }
-
-        if(cnt==n)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 }
