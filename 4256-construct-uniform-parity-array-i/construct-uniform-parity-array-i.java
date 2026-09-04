da498246c3aee57b051c1886686bb393b9ef9402
Rushikesh Dhakulkar <rushi53520dhakulkar@gmail.com>
2026-09-04 23:19:30 +0530

    Time: 1 ms (25.52%) | Memory: 45 MB (78.62%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,9 +1,31 @@
 class Solution {
-    public boolean uniformArray(int[] nums) {
+    public boolean uniformArray(int[] a) {
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0) {
-                return true;
+        int n = a.length;
+        int j = -1;
+
+        // Step 1: Find one odd number
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 != 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no odd number exists,
+        // all numbers are already even
+        if (j == -1) {
+            return true;
+        }
+
+        // Step 2: Make every even number odd
+        for (int i = 0; i < n; i++) {
+
+            if (a[i] % 2 == 0) {
+                int result = a[i] - a[j];
+
+                // result will be odd
+                // so this element can be made odd
             }
         }
 
